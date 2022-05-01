49f1e42362b3036b017323d27258a2f7d7dda1e2
yaremchuken <yaremchuken@yandex-team.ru>
2022-05-01 21:38:08 +0300

    Tab adviser

--- src/components/burger-ingredients/BurgerIngredients.jsx
+++ src/components/burger-ingredients/BurgerIngredients.jsx
@@ -1,5 +1,5 @@
 import { Counter, CurrencyIcon, Tab } from '@ya.praktikum/react-developer-burger-ui-components';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { CHOOSE_INGREDIENT } from '../../services/actions/ingredients';
 import styles from './burger-ingredients.module.css';
@@ -16,9 +16,35 @@ const BurgerIngredients = () => {
   const { ingredients } = useSelector((store) => store.ingredients);
 
   const [current, setCurrent] = useState('buns');
+  const [holderOffset, setHolderOffset] = useState(0);
+
+  const bunRef = useRef();
+  const sauceRef = useRef();
+  const mainRef = useRef();
 
   const onIngredientChoose = (id) => {
     dispatch({ type: CHOOSE_INGREDIENT, ingredient: ingredients.find((i) => i._id === id) });
+  };
+
+  useEffect(() => {
+    if (bunRef && holderOffset === 0) {
+      setHolderOffset(bunRef.current.getBoundingClientRect().y);
+    }
+  }, [bunRef, holderOffset]);
+
+  // Выделяем таб содержимое которого сейчас занимает основное экранное пространство (ближе всего к верхней границе)
+  const tabAdviser = () => {
+    const closest = [getYOffset('buns', bunRef), getYOffset('sauces', sauceRef), getYOffset('mains', mainRef)].reduce(
+      (prev, curr) => (curr.yPos < prev.yPos ? curr : prev)
+    ).key;
+
+    if (current !== closest) {
+      setCurrent(closest);
+    }
+  };
+
+  const getYOffset = (key, ref) => {
+    return { key, yPos: Math.abs(ref.current.getBoundingClientRect().y - holderOffset) };
   };
 
   return (
@@ -43,12 +69,13 @@ const BurgerIngredients = () => {
           </Tab>
         </a>
       </div>
-      <div className={styles.ingredientList}>
+      <div className={styles.ingredientList} onScroll={tabAdviser}>
         {types.map((type) =>
           toIngredientsSection(
             ingredients.filter((d) => d.type === type.key),
             type.name,
-            onIngredientChoose
+            onIngredientChoose,
+            type.key === 'bun' ? bunRef : type.key === 'sauce' ? sauceRef : mainRef
           )
         )}
       </div>
@@ -56,9 +83,9 @@ const BurgerIngredients = () => {
   );
 };
 
-const toIngredientsSection = (ingredients, name, clickHandler) => {
+const toIngredientsSection = (ingredients, name, clickHandler, ref) => {
   return (
-    <div className={`${styles.ingredientSection} pt-10`} key={name} id={name}>
+    <div className={`${styles.ingredientSection} pt-10`} key={name} id={name} ref={ref}>
       <p className={`${styles.ingredientType} text text_type_main-medium`}>{name}</p>
       <ul className={styles.ingredients}>{ingredients.map((i) => toIngredientCard(i, clickHandler, 0))}</ul>
     </div>
